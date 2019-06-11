7275fc02b4443df8ab17c9b7e5c93e4dfa17a09c
Eloge <eloge.ndja@uamaker.nyc>
2019-06-11 11:23:25 -0400

    created window with josh

---
+++ mightLike 01.py
@@ -0,0 +1,12 @@
+from graphics import*
+
+#setting window size
+shapesWin = GraphWin ("shapes", 700, 700)
+shapesWin.setCoords (0,0,500,500)
+
+winX = 500
+winY = 500
+
+#window
+shapesWin.getMouse()
+shapesWin.close()
