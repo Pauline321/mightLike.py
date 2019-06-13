d95b9d9768256270c5f54e3542a171ce2cb28479
Eloge <eloge.ndja@uamaker.nyc>
2019-06-13 16:55:29 -0400

    me and josh worked on turning making functions for the rectangles and are now working on the continue button

--- mightLike 01.py
+++ mightLike 01.py
@@ -7,59 +7,101 @@ shapesWin.setCoords (0,0,500,500)
 winX = 500
 winY = 500
 
-#rectangle
-#Rec1
-pSq = Rectangle (Point(0, 450), Point(500,500))
-pSq.setFill(color_rgb(250,250, 250))
-pSq.draw(shapesWin)
+#fuction for rectangle
+def draw_Rec(rX, rY, size, color, win):
+    rectangle = Rectangle(rX, rY)
+    rectangle.setFill(color)
+    rectangle.draw(win)
 
-#Input1
+def continueButton(rX, rY, text, txSize, txCol, style, win):
+    #nextRec = Rectangle(Point(rX , rY), Point(rX, rY))
+    #nextRec.setFill(rCol)
+    #nextRec.draw(win)
+
+    nextTex = Text(Point(rX, rY), text)
+    nextTex.setSize(txSize)
+    nextTex.setStyle(style)
+    nextTex.setTextColor(txCol)
+    nextTex.draw(win)
+    
+rX = 400
+rY = 30
+text = "Continue"
+txSize = 15
+rCol = color_rgb(0, 255, 0)
+txCol = "black"
+style = "bold"
+
+#Rec1
+draw_Rec(Point(0,450), Point(500,500), 50, "white", shapesWin)
+
+message = Text(Point (100, 475) , "What is the title of your favorite song?")
+message.draw(shapesWin)
+
 inputBox = Entry(Point(400,475), 20)
 inputStr = inputBox.getText()
+inputBox.setTextColor("Black")
+inputBox.draw(shapesWin)
+
+#Rec2
+draw_Rec(Point(0,400), Point(500,350), 50, "white", shapesWin)
+
+
+message = Text(Point (100, 375) , "Who is the creator of your song?")
+message.draw(shapesWin)
+
+inputBox = Entry(Point(400,375), 20)
+inputStr = inputBox.getText()
+inputBox.setTextColor("Black")
+inputBox.draw(shapesWin)
+
+#Rec3
+draw_Rec(Point(0,300), Point(500,250), 50, "white", shapesWin)
+
+message = Text(Point (100, 275) , "Who is the performer on the song?")
+message.draw(shapesWin)
+
+inputBox = Entry(Point(400,275), 20)
+inputStr = inputBox.getText()
+inputBox.setTextColor("gray")
+inputBox.draw(shapesWin)
+
+#Rec4
+draw_Rec(Point(0,200), Point(500,150), 50, "white", shapesWin)
+
+message = Text(Point (100, 175) , "In what language is the song in?")
+message.draw(shapesWin)
+
+
+inputBox = Entry(Point(400,175), 20)
+inputStr = inputBox.getText()
+inputBox.setTextColor("Black")
+inputBox.draw(shapesWin)
+
+#Rec5
+draw_Rec(Point(0,100), Point(500,50), 50, "white", shapesWin)
+
+message = Text(Point (100, 75) , "In what language is the song in?")
+message.draw(shapesWin)
+
+inputBox = Entry(Point(400,75), 20)
+inputStr = inputBox.getText()
+inputBox.setTextColor("Black")
+inputBox.draw(shapesWin)
+
+# continue button
+continueButton(rX, rY, text, txSize, txCol, style, shapesWin)
+#window
+shapesWin.getMouse()
+shapesWin.close()
+
+#Input1
+#inputBox = Entry(Point(400,475), 20)
+#inputStr = inputBox.getText()
 #inputBox.setText("32.0")
 #inputBox.setFace("courier")
 #inputBox.setSize(25)
 #inputBox.setStyle("italic")
-inputBox.setTextColor("Black")
+#inputBox.setTextColor("Black")
 #inputBox.setFill("white")
-inputBox.draw(shapesWin)
-
-#Rec2
-pSq = Rectangle (Point(0, 400), Point(500,350))
-pSq.setFill(color_rgb(250,250, 250))
-pSq.draw(shapesWin)
-#Rec3
-pSq = Rectangle (Point(0, 300), Point(500,250))
-pSq.setFill(color_rgb(250,250, 250))
-pSq.draw(shapesWin)
-#Rec4
-pSq = Rectangle (Point(0, 200), Point(500,150))
-pSq.setFill(color_rgb(250,250, 250))
-pSq.draw(shapesWin)
-#Rec5
-pSq = Rectangle (Point(0, 100), Point(500,50))
-pSq.setFill(color_rgb(250,250, 250))
-pSq.draw(shapesWin)
-
-#text
-#Box1
-message = Text(Point (100, 475) , "What is the title of your favorite song?")
-message.draw(shapesWin)
-
-
-#Box2
-message = Text(Point (100, 375) , "Who is the creator of your song?")
-message.draw(shapesWin)
-#Box3
-message = Text(Point (100, 275) , "Who is the performer on the song?")
-message.draw(shapesWin)
-#Box4
-message = Text(Point (100, 175) , "In what language is the song in?")
-message.draw(shapesWin)
-#Box4
-message = Text(Point (100, 75) , "In what language is the song in?")
-message.draw(shapesWin)
-
-#window
-shapesWin.getMouse()
-shapesWin.close()
+#inputBox.draw(shapesWin)
